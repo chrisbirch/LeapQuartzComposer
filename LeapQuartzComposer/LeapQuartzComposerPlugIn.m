2342b1dff2c64cc9abfc334c6eacaefdc825fa89
Chris Birch <chris@ocastastudios.com>
2013-04-21 20:56:14 +0100

    version number bump

--- LeapQuartzComposer/LeapQuartzComposerPlugIn.m
+++ LeapQuartzComposer/LeapQuartzComposerPlugIn.m
@@ -14,7 +14,7 @@
 
 
 #define	kQCPlugIn_Name				@"Leap Device Interface SDK 0.7.8"
-#define	kQCPlugIn_Description		@"Version: 0.35\nAllows QC compositions to access data returned by Leap Motion devices"
+#define	kQCPlugIn_Description		@"Version: 0.36\nAllows QC compositions to access data returned by Leap Motion devices"
 #define kQCPlugIn_AuthorDescription @"© 2013 by Chris Birch, all rights reserved."
 
 @interface LeapQuartzComposerPlugIn ()
